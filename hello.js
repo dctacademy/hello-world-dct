370646fd140da3e19708d33f8ab1708e0d86688b
Arpitha HK <arphk286@gmail.com>
2019-01-03 17:42:55 +0530

    Added /msg Route and created a new file

--- hello.js
+++ hello.js
@@ -2,7 +2,9 @@ const http = require('http')
 const server= http.createServer(function(request,response){
     if(request.url==='/'){
         response.end('hello world')
-    }
+    } else if (request.url === '/msg' ) {
+        response.end('Good Evening')
+    } 
 }
 )
 server.listen(3000,function(){
