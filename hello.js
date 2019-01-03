8cee1161c9523dd16ac80bd4cd2c4db8b47c6f9f
svamshikrishna3 <svamshikrishna3@gmail.com>
2019-01-03 17:43:19 +0530

    add contact and about page/ alsoadded index.js

--- hello.js
+++ hello.js
@@ -2,6 +2,11 @@ const http = require('http')
 const server= http.createServer(function(request,response){
     if(request.url==='/'){
         response.end('hello world')
+
+    } else if(request.url === '/contact'){
+        response.end('contact information');
+    } else if (request.url === '/about'){
+        response.end('you have come to about page')
     }
 }
 )
