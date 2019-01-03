b35434e5223e5024bb8d00c078b0bca4bb722464
saima afroz <saimaafroz73@gmail.com>
2019-01-03 17:43:26 +0530

    my page added

--- hello.js
+++ hello.js
@@ -2,9 +2,12 @@ const http = require('http')
 const server= http.createServer(function(request,response){
     if(request.url==='/'){
         response.end('hello world')
-    }
-}
-)
+    } else if(request.url === '/saima'){
+        response.end('This page belongs to saima')
+    } 
+    
+})
+
 server.listen(3000,function(){
     console.log('server listen at 3000')
 })
